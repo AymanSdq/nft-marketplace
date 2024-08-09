5b6f029108440fc00e1a80f44074fa641d2256f0
AymanSdq <sedqiayman02@gmail.com>
2024-08-09 22:33:38 +0100

    Adding Fonts

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,7 +7,22 @@ function HomePage() {
         
         <>
             <section>
+                {/* Header Section */}
                 <Header />
+                {/* Hero Section */}
+                <div className='w-full h-screen flex px-48'>
+                    {/* Right section */}
+                    <div className='w-1/2 flex flex-col gap-8'>
+                        <h1 className='text-4xl text-white'>Discover Digital Art & Collect NFTs</h1>
+                        <p className='text-white'>NFT marketplace UI created with Anima for Figma. Collect, buy and sell art from more than 20k NFT artists.</p>
+                    </div>
+                    {/* Left section */}
+                    <div className='w-1/2 flex flex-col'>
+
+                    </div>
+
+                </div>
+
             </section>
         </>
 
